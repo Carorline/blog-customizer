7960b02008bf1bfbfecb6f2b733b3d083976834c
Valentina Osinceva <Carorline@yandex.ru>
2025-08-18 22:57:35 +0300

    Выполнен функционал откр/закр сайдбара. Добавлен выбор шрифта.

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,47 @@
 import { ArrowButton } from 'src/ui/arrow-button';
+import { Select } from 'src/ui/select';
+import { fontFamilyOptions } from 'src/constants/articleProps';
 import { Button } from 'src/ui/button';
-
+import { useState, useRef, useEffect } from 'react';
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
 export const ArticleParamsForm = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	const sidebarRef = useRef<HTMLFormElement>(null);
+
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false);
+			}
+		};
+
+		if (isOpen) {
+			document.addEventListener('mousedown', handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+		};
+	}, [isOpen]);
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={isOpen} onClick={() => setIsOpen(!isOpen)} />
+			<aside
+				ref={sidebarRef}
+				className={clsx(styles.container, {
+					[styles.container_open]: isOpen,
+				})}>
 				<form className={styles.form}>
+					<Select
+						title='Шрифт'
+						options={fontFamilyOptions}
+						selected={fontFamilyOptions[0]}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
